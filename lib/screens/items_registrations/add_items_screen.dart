3520cb11281cba55ceba6b1c62bc8f77d35f9107
Fine Traders <zohnirtariq@gmail.com>
2024-03-06 16:35:30 +0500

    regs

--- lib/screens/items_registrations/add_items_screen.dart
+++ lib/screens/items_registrations/add_items_screen.dart
@@ -11,40 +11,48 @@ import '../../helper/text_helper.dart';
 import '../dashboard/components/header.dart';
 
 class AddItemsScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    final provider =   Provider.of<MenuAppController>(context, listen: false);
+    final provider = Provider.of<MenuAppController>(context, listen: false);
     return SafeArea(
         child: SingleChildScrollView(
-          primary: false,
-          padding: const EdgeInsets.all(defaultPadding),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Header(),
-              const SizedBox(height: defaultDrawerHeadHeight + 20.0),
-              TextHelper().mNormalText(text: "Items",color: Colors.white,size: 18.0),
-              const SizedBox(height: defaultDrawerHeadHeight-10.0),
-              TextHelper().mNormalText(text: "Add new Items",color: Colors.white70,size: 14.0),
-              const SizedBox(height: defaultDrawerHeadHeight + 20.0),
-              ItemsRegistrationForm(
-                edit: provider.parameters?['edit'] ?? 'false',
-                code: provider.parameters?[Constant.KEY_ITEM_CODE] ?? "no code",
-                name: provider.parameters?[Constant.KEY_ITEM_NAME] ?? 'Enter Item Name',
-                category: provider.parameters?[Constant.KEY_ITEM_CATEGORY] ?? 'Enter category',
-                uom: provider.parameters?[Constant.KEY_ITEM_UOM] ?? 'Enter uom',
-                stock: provider.parameters?[Constant.KEY_ITEM_STOCK] ?? 'Enter stock',
-                quantity: provider.parameters?[Constant.KEY_ITEM_QUANTITY] ?? 'Enter quantity',
-                purchasePrice: provider.parameters?[Constant.KEY_ITEM_PURCHASE_PRICE] ?? 'Enter Purchase Price',
-                salePrice: provider.parameters?[Constant.KEY_ITEM_SALE_PRICE] ?? 'Enter Sale Price',
-                joinDate: provider.parameters?[Constant.KEY_VENDORMAN_JOIN_DATE] ?? 'select date',
-                status: provider.parameters?[Constant.KEY_STATUS] ?? 'choose status',
-              )
-            ],
-          ),
-        )
-    );
+      primary: false,
+      padding: const EdgeInsets.all(defaultPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Header(),
+          const SizedBox(height: defaultDrawerHeadHeight + 20.0),
+          TextHelper()
+              .mNormalText(text: "Items", color: Colors.white, size: 18.0),
+          const SizedBox(height: defaultDrawerHeadHeight - 10.0),
+          TextHelper().mNormalText(
+              text: "Add new Items", color: Colors.white70, size: 14.0),
+          const SizedBox(height: defaultDrawerHeadHeight + 20.0),
+          ItemsRegistrationForm(
+            edit: provider.parameters?['edit'] ?? 'false',
+            code: provider.parameters?[Constant.KEY_ITEM_CODE] ?? "no code",
+            name: provider.parameters?[Constant.KEY_ITEM_NAME] ??
+                'Enter Item Name',
+            category: provider.parameters?[Constant.KEY_ITEM_CATEGORY] ??
+                'Enter category',
+            uom: provider.parameters?[Constant.KEY_ITEM_UOM] ?? 'Enter uom',
+            stock:
+                provider.parameters?[Constant.KEY_ITEM_STOCK] ?? 'Enter stock',
+            quantity: provider.parameters?[Constant.KEY_ITEM_QUANTITY] ??
+                'Enter quantity',
+            purchasePrice:
+                provider.parameters?[Constant.KEY_ITEM_PURCHASE_PRICE] ??
+                    'Enter Purchase Price',
+            salePrice: provider.parameters?[Constant.KEY_ITEM_SALE_PRICE] ??
+                'Enter Sale Price',
+            joinDate: provider.parameters?[Constant.KEY_VENDORMAN_JOIN_DATE] ??
+                'select date',
+            status:
+                provider.parameters?[Constant.KEY_STATUS] ?? 'choose status',
+          )
+        ],
+      ),
+    ));
   }
 }
